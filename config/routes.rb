674e7ef1e855d101fb9635921d5420d2e163f362
si <simonlindeque@gmail.com>
2016-01-23 16:10:45 +0000

    create templates n stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#homepage'
+
   get 'about', to: 'pages#about'
+  
+  resource :articles
   
   
   # Example of regular route:
